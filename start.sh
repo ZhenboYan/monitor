77986d491ab6796d84d2648ebf48522b671d0207
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-13 10:45:41 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+sudo docker run -d --name startprom --net host -p 9090:9090 -v $PWD/PrometheusGrafana/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:v2.2.1
+docker run -d --name startgrafana -p 3000:3000 -e "GF_INSTALL_PLUGINS=jdbranham-diagram-panel" grafana/grafanasudo 
+docker run -d --name startpush -p 9091:9091 prom/pushgateway
+
+cd PrometheusGrafana
+
+python3 dynamic.py topFlowConfig,yml
