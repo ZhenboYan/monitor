52f55a6e21c2d8b9f70a267c1bdb5e03855a7257
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-13 11:23:47 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 pip install pyymal
 pip install requests
 
-sudo docker run -d --name startprom --net host -p https://localhost:9090:9090 -v $PWD/PrometheusGrafana/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:v2.2.1
-sudo docker run -d --name startgrafana -p 3000:3000 -e "GF_INSTALL_PLUGINS=jdbranham-diagram-panel" grafana/grafana
+sudo docker run -d --name startprom --net host -p 9090:9090 -v $PWD/PrometheusGrafana/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus:v2.2.1
+sudo docker run -d --name startgrafana -p 3000:https://localhost:3000 -e "GF_INSTALL_PLUGINS=jdbranham-diagram-panel" grafana/grafana
 sudo docker start startgrafana
 sudo docker run -d --name startpush -p 9091:9091 prom/pushgateway
 
