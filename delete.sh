7120732deb8a3cc89c05dfc052eca09a6c542605
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-14 17:23:42 -0400

    Update delete.sh

--- delete.sh
+++ delete.sh
@@ -2,5 +2,4 @@
 echo "!!    Delete pushgateway and promethues containers"
 echo "!!    Stop grafana-server service"
 docker rm -f startpush startprom startgrafana
-rm -f PrometheusGrafana/prometheus.yml
 sudo systemctl stop grafana-server
