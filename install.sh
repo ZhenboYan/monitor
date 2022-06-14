be081ad45b164c4b01b59eccef33597d8c9ba3cb
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-14 14:28:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
+# START
+echo "|| Grafana-Prometheus-Pushgateway Installer ||"
+echo "||                                          ||"
 
+## Read inputs
+while getopts l: flag; do
+    case "${flag}" in
+    l) input_lets=${OPTARG} ;;
+    esac
+done
+
+# install grafana
 sudo tee  /etc/yum.repos.d/grafana.repo<<EOF
 [grafana]
 name=grafana
@@ -11,11 +22,25 @@ gpgkey=https://packages.grafana.com/gpg.key
 sslverify=1
 sslcacert=/etc/pki/tls/certs/ca-bundle.crt
 EOF
-
 sudo yum  -y install grafana
-
 sudo systemctl enable --now grafana-server
-
 sudo firewall-cmd --add-port=3000/tcp --permanent
-
 sudo firewall-cmd --reload
+
+# >Certificates
+
+if [ -z "$input_lets" ]; then
+    echo "Let's Encrypt Certificate Setup: "
+    echo "      1) Let's Encrypt signed certificate. (this machine must be reachable via over the internet by the domain name)"
+    echo "      2) Finish Install."
+    read -r -p "Select a mode [1]: " sslmode
+    sslmode=${sslmode:-1}
+else
+    echo "Let's Encrypt command-line input found."
+    sslmode=${input_lets:-1}
+fi
+if [ "$sslmode" == "1" ]; then # Let's Encrypt
+    echo "    Note: port 80 must be available for DNS challenges to succeed. "
+    echo "          See https://certbot.eff.org/faq for more information."
+    pause
+fi
