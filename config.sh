b9f9e5875c4090d9c592c1c3c3edf0c429c6084b
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-15 13:53:19 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 MYIP=$(hostname -I | head -n1 | awk '{print $1;}')
 
-read -r -p "Is this your IP address ${MYIP} [y/N]" correct_ip
+read -r -p "Is ${MYIP} your IP address [y/N]: " correct_ip
 
 if [ "$correct_ip" == "N" ]; then
     read -r -p "Type in your ip address: " MYIP
@@ -11,10 +11,10 @@ echo "||    Inserting your IP address ${MYIP} to prometheus.yml file"
 echo "||    If the IP address is incorrect please update manually"
 sed -i -e "s@your_ip:9091 @${MYIP}:9091 @" PrometheusGrafana/prometheus.yml
 
-echo "!!    Make sure you have configured the config file before runing start.sh"
-echo "!!    PrometheusGrafana/config.yml"
-echo "!!    Visit Google Doc for API instruction"
-echo "!!    https://docs.google.com/document/d/e/2PACX-1vRAwtpqlMKbii-hiqMoFD_N5PghMSw2eTMts9VhBww3AoSnXnQkjEcra4ReyLLsXrAuE_VEwLHRg33c/pub"
+echo " "
+echo "!!    MANUAL CONFIGURATION STEP"
+echo "!!    Make sure you have configured PrometheusGrafana/config.yml before ./start.sh"
 echo "!!    configuration templates are provided PrometheusGrafana/*_template.yml"
-echo "vim PrometheusGrafana/config.yml"
-echo "nano PrometheusGrafana/config.yml"
+echo "!!    Visit Google Doc for Grafana API Key instruction: https://docs.google.com/document/d/e/2PACX-1vRAwtpqlMKbii-hiqMoFD_N5PghMSw2eTMts9VhBww3AoSnXnQkjEcra4ReyLLsXrAuE_VEwLHRg33c/pub"
+
+echo "after configuration run ./start.sh to start Grafana-Prometheus-Pushgateway"
