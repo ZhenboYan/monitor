8ccd4207345591620484294be57cbb3fac6fb4b9
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-15 14:17:56 -0400

    add host number

--- start.sh
+++ start.sh
@@ -14,27 +14,24 @@ docker rm -f startpush startprom
 # sudo docker run -d --name startgrafana -p 3000:3000 -e "GF_INSTALL_PLUGINS=jdbranham-diagram-panel" grafana/grafana
 # sudo docker start startgrafana
 
-echo "!!    Number of hosts"
-echo "!!    1). 1       2). 2       3). 3 or more"
+read -r -p "!!    Input config file name: " config_file
 
-read -r -p "Select: " hostnum
-
-if [ "$hostnum" == "1" ] || [ "$hostnum" == "2" ] || [ "$hostnum" == "3" ]; then
+if [ -f "PrometheusGrafana/$config_file" ]; then
     sudo systemctl start grafana-server
     cd PrometheusGrafana
     sudo docker run -d --name startprom -p 9090:9090     -v $PWD/prometheus.yml:/etc/prometheus/prometheus.yml     prom/prometheus:v2.2.1
     sudo docker run -d --name startpush -p 9091:9091 prom/pushgateway
-else 
-    echo "!!    Invalid selection"
+    python3 two_dynamic.py $config_file
+    # if [ "$hostnum" == "1" ]; then
+    # python3 one_dynamic.py $config_file
+    # fi
+    # if [ "$hostnum" == "2" ]; then
+    #     python3 two_dynamic.py $config_file
+    # fi
+    # if [ "$hostnum" == "3" ]; then
+    #     python3 multi_dynamic.py $config_file
+    # fi
+else
+    echo "!!    Config file doesn't exist"
     exit 1
-fi
-
-if [ "$hostnum" == "1" ]; then
-    python3 one_dynamic.py config.yml 
-fi
-if [ "$hostnum" == "2" ]; then
-    python3 two_dynamic.py config.yml 
-fi
-if [ "$hostnum" == "3" ]; then
-    python3 multi_dynamic.py config.yml 
 fi
