616f0e19007acba8831ff7bf898b53da6bafe8a2
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2022-06-17 13:49:15 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,14 +11,16 @@ echo "||    Inserting ${MYIP} to prometheus.yml file"
 echo "||    If the IP address is incorrect please update manually"
 sed -i -e "s@your_ip:9091 @${MYIP}:9091 @" PrometheusGrafana/prometheus.yml
 
+sleep 1
 echo " "
 echo "!!    MANUAL CONFIGURATION STEP"
 echo "!!    Make sure you have configured PrometheusGrafana/config.yml before ./start.sh"
+sleep 0.5
 echo "!!    configuration templates are provided PrometheusGrafana/*_template.yml"
 echo "!!    Visit Google Doc for Grafana API Key instruction: https://docs.google.com/document/d/e/2PACX-1vRAwtpqlMKbii-hiqMoFD_N5PghMSw2eTMts9VhBww3AoSnXnQkjEcra4ReyLLsXrAuE_VEwLHRg33c/pub"
-
+sleep 0.5
 echo "!!    After Configuration: run ./start.sh to start Grafana-Prometheus-Pushgateway"
 echo " "
-
-read -p "!!     Press enter to continue"
+sleep 0.5
+read -p "Press enter to continue"
 echo " "
